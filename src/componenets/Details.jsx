58454f26eeddbd78c24eb7c9fe8656df3b29f2df
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2025-01-15 09:00:46 +0200

    (by instructor)

--- src/componenets/Details.jsx
+++ src/componenets/Details.jsx
@@ -2,34 +2,30 @@ import instructorImg from "../images/instructor.jpg";
 
 export default function Details() {
   return (
-    <section>
+    <section id="discover" className="details py-6 bg-light">
       <div className="container">
-        <div className="row py-6">
+        <div className="row">
           <div className="col-lg-6">
-            <div className="image-container">
-              <img
-                src={instructorImg}
-                alt="Instructor photo"
-                className="rounded-5 img-fluid"
-              />
-            </div>
+            <img
+              src={instructorImg}
+              alt="Instructor photo"
+              className="rounded-5 img-fluid mb-4"
+            />
           </div>
-          <div className="col-lg-6">
-            <div className="text-container">
-              <h2>Enhance Your Skills with Engaging Training Videos</h2>
-              <p>
-                Our extensive collection of training videos is designed to help
-                you enhance your skills and excel in your chosen field. Whether
-                you're looking to improve your technical expertise or develop
-                essential soft skills, our videos provide valuable insights and
-                practical knowledge.
-              </p>
-              <p>
-                With expert instructors and comprehensive content, you'll gain
-                the confidence and competence to tackle challenges and achieve
-                success.
-              </p>
-            </div>
+          <div className="col-lg-6 d-flex flex-column justify-content-center">
+            <h2>Enhance Your Skills with Engaging Training Videos</h2>
+            <p>
+              Our extensive collection of training videos is designed to help
+              you enhance your skills and excel in your chosen field. Whether
+              you're looking to improve your technical expertise or develop
+              essential soft skills, our videos provide valuable insights and
+              practical knowledge.
+            </p>
+            <p>
+              With expert instructors and comprehensive content, you'll gain the
+              confidence and competence to tackle challenges and achieve
+              success.
+            </p>
           </div>
         </div>
       </div>
--- src/componenets/Register.jsx
+++ src/componenets/Register.jsx
@@ -3,39 +3,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Register() {
   return (
-    <section className="bg-primary">
+    <section className="register bg-primary py-6 overflow-hidden">
       <div className="container">
-        <div className="row py-6">
+        <div className="row">
           <div className="col-lg-6">
-            <div className="text-container">
-              <h2>Unlock Your Potential with Engaging Content</h2>
-              <p>
-                Our wide range of training and seminar videos are designed to
-                empower you with valuable knowledge and skills.
-              </p>
-              <ul className="list-unstyled">
-                <li className="mb-2">
-                  <FontAwesomeIcon icon={faSquare} />
-                  <strong> Gain Expertise:</strong> Our videos provide in-depth
-                  insights and practical tips to enhance your expertise in
-                  various domains.
-                </li>
-                <li className="mb-2">
-                  <FontAwesomeIcon icon={faSquare} />
-                  <strong> Stay Updated:</strong> Stay up-to-date with the
-                  latest industry trends and advancements through our
-                  informative and up-to-date videos.
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faSquare} />
-                  <strong> Expand Your Network:</strong> Connect with
-                  like-minded individuals and industry experts, fostering new
-                  connections and opportunities.
-                </li>
-              </ul>
-            </div>
+            <h2>Unlock Your Potential with Engaging Content</h2>
+            <p>
+              Our wide range of training and seminar videos are designed to
+              empower you with valuable knowledge and skills.
+            </p>
+            <ul className="list-unstyled vstack gap-3">
+              <li>
+                <FontAwesomeIcon icon={faSquare} />
+                <strong> Gain Expertise:</strong> Our videos provide in-depth
+                insights and practical tips to enhance your expertise in various
+                domains.
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faSquare} />
+                <strong> Stay Updated:</strong> Stay up-to-date with the latest
+                industry trends and advancements through our informative and
+                up-to-date videos.
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faSquare} />
+                <strong> Expand Your Network:</strong> Connect with like-minded
+                individuals and industry experts, fostering new connections and
+                opportunities.
+              </li>
+            </ul>
           </div>
-          <div className="col-lg-6">
+          <div className="col-lg-6 p-4">
             <form>
               <div className="my-4">
                 <input
@@ -55,7 +53,7 @@ export default function Register() {
               </div>
               <div className="my-4">
                 <input
-                  type="number"
+                  type="text"
                   name="phone-number"
                   className="form-control form-control-lg rounded-0 border-0"
                   placeholder="Enter phone number"
@@ -66,9 +64,9 @@ export default function Register() {
                   class="form-check-input"
                   type="checkbox"
                   value=""
-                  id="flexCheckDefault"
+                  id="terms"
                 />
-                <label class="form-check-label" for="flexCheckDefault">
+                <label class="form-check-label" for="terms">
                   I agree to the terms and conditions
                 </label>
               </div>

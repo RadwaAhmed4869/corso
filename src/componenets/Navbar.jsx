56a6fc06f3922cdf9c1f20087fefef4f6f8c8e49
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2025-01-16 04:42:57 +0200

    fix navbar icons

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" aria-current="page" href="#home">
                 Home
@@ -51,11 +51,16 @@ export default function Navbar() {
               </a>
             </li>
           </ul>
-          <span className="nav-item">
+          <span className="nav-item ms-lg-4 me-4">
             <span className="fa-layers fa-fw">
               <a href="#">
                 <FontAwesomeIcon icon={faCircle} size="2x" />
-                <FontAwesomeIcon icon={faFacebookF} color={"white"} size="1x" />
+                <FontAwesomeIcon
+                  icon={faFacebookF}
+                  color={"white"}
+                  size="1x"
+                  className="ps-2"
+                />
               </a>
             </span>
           </span>
@@ -63,7 +68,12 @@ export default function Navbar() {
             <span className="fa-layers fa-fw">
               <a href="#">
                 <FontAwesomeIcon icon={faCircle} size="2x" />
-                <FontAwesomeIcon icon={faTwitter} color={"white"} size="1x" />
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  color={"white"}
+                  size="1x"
+                  className="ps-2"
+                />
               </a>
             </span>
           </span>
